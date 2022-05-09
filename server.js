49b78f2c295beb5d5cd53d582f2c710145e3757a
Spencer Baker <spencer.baker77@gmail.com>
2022-05-09 13:16:29 -0600

    fixed server.js file

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ app.listen(port, ()=>{
     console.log("listening...")
 });
 
-app.get('/',(req,res)=>{//every time something calls your API, that is a request
-    res.send("HOLY CRAP ITS WORKING!")//a response is when an API gives the inofrmation requested
+app.post('/login',(req,res)=>{//a post is when a client sends information to an API
+    console.log('Request Body',JSON.stringify(req.body));
+    const loginRequest = req.body;
+    if (loginRequest.userName=="jacobjacob@gmail.com" && loginRequest.password=="P@ssw0rd"){
+        res.status(200);
+        res.send("Welcome");
+    } else{
+        res.status(401);
+        res.send("Unauthorized");
+        }
 });
 
-app.post('/login',(req,res)=>{//a post is when a client sends information to an API
-    console.log('Request Body', JSON.stringify(request.body));
-    const loginRequest = request.body;
-    if (loginRequest.userName=="jacobjacob@gmail.com" && loginRequest.password=="P@ssw0rd"){
-        response.status(200);
-        response.send("Welcome");
-    } else{
-        response.status(401);
-        response.send("Unauthorized");
-        }
+app.get('/',(req,res)=>{//every time something calls your API, that is a request
+    res.send("HOLY CRAP ITS WORKING!")//a response is when an API gives the inofrmation requested
 });
