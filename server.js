b53c3e5ea326ccf6a6a571428325750be5f9e048
Spencer Baker <spencer.baker77@gmail.com>
2022-05-11 13:46:06 -0600

    Added redis references for Friday

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');//import the library
 const port = 3000;
 const bodyParser = require('body-parser');//body-parder is called middleware
 const app = express();//use the library
+const md5 = require('md5');
 
 app.use(bodyParser.json());//use the middleware (call it before anything else happens on each request)
 
@@ -12,6 +13,9 @@ app.listen(port, ()=>{
 app.post('/login',(req,res)=>{//a post is when a client sends information to an API
     console.log('Request Body',JSON.stringify(req.body));
     const loginRequest = req.body;
+    //search database for username and retrieve current password
+    const hashedPasswordFromUser = md5(req.body.password)
+    //compare the hashed version of the password that was sent with the hashed version from the database
     if (loginRequest.userName=="jacobjacob@gmail.com" && loginRequest.password=="P@ssw0rd"){
         res.status(200);
         res.send("Welcome");
