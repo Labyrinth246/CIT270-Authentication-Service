2f8dcb19340129175718f5c5739cc6c9811df0b6
Spencer Baker <spencer.baker77@gmail.com>
2022-06-08 12:54:21 -0600

    added url to .bat files and added correct certification keys as well as a passphrase in server.js

--- apiclientsignup.bat
+++ apiclientsignup.bat
@@ -1,1 +1,1 @@
-curl -d "@datasignup.json" -X POST -H "Content-Type:application/json" http://localhost:3000/signup
+curl -d "@datasignup.json" -X POST -H "Content-Type:application/json" https://cit270.com:3000/signup
--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());//use the middleware (call it before anything else ha
 https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert'),
+    passphrase: 'P@ssw0rd',
 }, app).listen(port, async ()=>{
     await redisClient.connect();
     console.log("listening on port:"+port);
